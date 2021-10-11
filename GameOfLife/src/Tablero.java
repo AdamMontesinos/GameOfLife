9c77b3ce03e2b6c6f5711f44dc43be20a224038d
Adam <58786408+AdamMontesinos@users.noreply.github.com>
2021-10-11 15:15:42 +0200

    Add files via upload

--- GameOfLife/src/Tablero.java
+++ GameOfLife/src/Tablero.java
@@ -5,8 +5,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
+import org.w3c.dom.events.MouseEvent;
+
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.JButton;
 
 public class Tablero {
 
@@ -43,10 +47,23 @@ public class Tablero {
 		int rows = 3;
 		int cols = 3;
 		int contador=0;
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 900, 900);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(rows,cols));
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panelBoton = new JPanel();
+		panelBoton.setBounds(0, 1, 900, 287);
+		frame.getContentPane().add(panelBoton);
+		
+		JButton btnNewButton = new JButton("New button");
+		panelBoton.add(btnNewButton);
+		
+		JPanel panelJuego = new JPanel();
+		panelJuego.setBounds(0, 288, 900, 287);
+		frame.getContentPane().add(panelJuego);
+		panelJuego.setLayout(new GridLayout(rows, cols));
 		
 		//Creem els taulell imaginari
 		int[][] game = new int[rows][cols];
@@ -60,7 +77,7 @@ public class Tablero {
 				Border borde;
 				borde = BorderFactory.createLineBorder(Color.black);
 				tablero[i][j].setBorder(borde);
-				frame.add(tablero[i][j]);
+				panelJuego.add(tablero[i][j]);
 			}
 		}
 		
@@ -68,7 +85,6 @@ public class Tablero {
 		game[0][1] = 1;
 		game[1][1] = 1;
 		game[2][1] = 1;
-		
 		
 		//Para imprimirlo por terminal
 		String out = "";
@@ -89,9 +105,7 @@ public class Tablero {
 			for(int j=0;j<cols;j++) {
 				
 				if(game[i][j]==1) {
-					tablero[i][j].setBackground(Color.red);
-					
-					
+					tablero[i][j].setBackground(Color.red); 
 				}else {
 					tablero[i][j].setBackground(Color.blue); 
 				}	
