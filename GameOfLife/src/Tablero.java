eaf216e2d98c04aee4c3b508f9eb29c342fbe7ec
Eric Salvatierra <49277893+martingrub@users.noreply.github.com>
2021-10-18 15:24:20 +0200

    Add files via upload

--- GameOfLife/src/Tablero.java
+++ GameOfLife/src/Tablero.java
@@ -16,9 +16,13 @@ import javax.swing.JSpinner;
 import javax.swing.JLabel;
 
 public class Tablero {
+	private final int rows = 3;
+	private final int cols = 3;
+	private int contador = 0;
 
 	private JFrame frame;
 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -40,6 +44,7 @@ public class Tablero {
 	 */
 	public Tablero() {
 		initialize();
+		
 	}
 
 	/**
@@ -47,9 +52,9 @@ public class Tablero {
 	 */
 	private void initialize() {
 		
-		int rows = 100;
-		int cols = 100;
-		int contador=0;
+		/*int rows = 3;
+		int cols = 3;
+		int contador=0;*/
 		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 900, 900);
@@ -63,17 +68,24 @@ public class Tablero {
 		panelBoton.setBounds(0, 811, 900, 52);
 		frame.getContentPane().add(panelBoton);
 		
+		
 		JPanel panelJuego = new JPanel();
-		panelJuego.setBounds(0, 0, 900, 900);
+		panelJuego.setBounds(0, 0, 900, 738);
 		frame.getContentPane().add(panelJuego);
 		panelJuego.setLayout(new GridLayout(rows, cols));
-		panelBoton.setLayout(null);
 		
 		//Botones y cosas
 		JButton btnPlay = new JButton("Play");
 		btnPlay.setBackground(Color.YELLOW);
 		btnPlay.setBounds(30, 15, 64, 25);
 		panelBoton.add(btnPlay);
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				inicializar(panelJuego, panelBoton);
+				contador=0;
+				
+			}
+		});
 		
 		JButton btnClear = new JButton("Clear");
 		btnClear.setBackground(Color.YELLOW);
@@ -116,28 +128,62 @@ public class Tablero {
 		
 		
 		
-		//Creem els taulell imaginari
-		int[][] game = new int[rows][cols];
-		JPanel [][] tablero = new JPanel[rows][cols];
-		for(int i=0; i<rows; i++) {
-			for(int j=0; j<cols; j++) {
-			
-				//Para Random
-				int red = 1;
-				int blue = 0;
-				
-				int random_int = (int)Math.floor(Math.random()*(red-blue+1)+blue);
-				game[i][j]= random_int;
-				
-				//Inicialitzamos el tablero visual sin color
-				tablero[i][j] = new JPanel();
-				Border borde;
-				borde = BorderFactory.createLineBorder(Color.black);
-				tablero[i][j].setBorder(borde);
-				panelJuego.add(tablero[i][j]);
-			}
-		}
 		
+		
+		//Print en Menu
+		lblCounter.setText(String.valueOf("Counter : " + contador));
+
+	}
+	private void inicializar(JPanel panelJuego, JPanel panelBoton) {
+		//Creem els taulell imaginari
+				int[][] game = new int[rows][cols];
+				JPanel [][] tablero = new JPanel[rows][cols];
+				for(int i=0; i<rows; i++) {
+					for(int j=0; j<cols; j++) {
+					
+						//Para Random
+						int red = 1;
+						int blue = 0;
+						
+						int random_int = (int)Math.floor(Math.random()*(red-blue+1)+blue);
+						game[i][j]= random_int;
+						
+						//Inicialitzamos el tablero visual sin color
+						tablero[i][j] = new JPanel();
+						Border borde;
+						borde = BorderFactory.createLineBorder(Color.black);
+						tablero[i][j].setBorder(borde);
+					
+						
+						panelJuego.add(tablero[i][j]);						
+						
+						
+					}
+					
+				}
+				this.frame.setVisible(true);
+
+
+				
+				imprimir_logico(game);
+				imprimir_visual(game, tablero);
+	}
+	
+	private void imprimir_visual(int[][] game, JPanel[][] tablero) {
+		//Los 1 pintamos rojo, los demas azul
+				for(int i=0;i<rows;i++){
+					for(int j=0;j<cols;j++) {
+						
+						if(game[i][j]==1) {
+							tablero[i][j].setBackground(Color.red); 
+						}else {
+							tablero[i][j].setBackground(Color.blue); 
+						}
+					}	
+				}
+	}
+	
+	private void imprimir_logico(int[][] game) {
 		//Para imprimirlo por terminal
 		String out = "";
 		for(int i=0; i<rows; i++) {
@@ -151,20 +197,5 @@ public class Tablero {
 		}
 		System.out.println(out);
 		System.out.println(contador);
-		
-		//Los 1 pintamos rojo, los demas azul
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<cols;j++) {
-				
-				if(game[i][j]==1) {
-					tablero[i][j].setBackground(Color.red); 
-				}else {
-					tablero[i][j].setBackground(Color.blue); 
-				}	
-			}
-		}
-		
-		//Print en Menu
-		lblCounter.setText(String.valueOf("Counter : " + contador));
 	}
 }
